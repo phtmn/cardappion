ffb6a7a3a78546306e1da997a21b14edc131ef82
Pedro Henrique Tolentino de Melo Nogueira <pedro.nogueira@hotmail.com.br>
2019-08-10 22:29:40 -0300

    ajustes

--- resources/views/site/show.blade.php
+++ resources/views/site/show.blade.php
@@ -2,14 +2,8 @@
 <html lang="en">
 <head>
     <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-75834608-2"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
 
-        gtag('config', 'UA-75834608-2');
-    </script>
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -25,67 +19,175 @@
 <body>
 <ion-app>
 
-    
+
     <div class="ion-page" main>
-        
+
 
         <ion-content padding>
 
         </ion-content>
         <ion-tabs>
             <ion-tab tab="cardapio">
+            <img src="{{asset('vendor/argon/assets/img/brand/google.jpg')}}" style="width: 400px; height:200px;">
                 <ion-content>
-               
-
-
-                
-
     </ion-conent>
+    <ion-list style="margin-top: 5px">
+
+                    <ion-item href="#">
+
+                        <ion-label>Nome da categoria cadastrada </br>e marcada como publicada</ion-label>
+                    </ion-item>
+
+                    <ion-item href="#">
+
+                        <ion-label>Nome da categoria</ion-label>
+                    </ion-item>
+                    <ion-item href="#">
+
+                        <ion-label>Nome da categoria</ion-label>
+                    </ion-item>
+                    <ion-item href="#">
+
+                        <ion-label>Nome da categoria</ion-label>
+                    </ion-item>
+                    <ion-item href="#">
+
+                        <ion-label>Nome da categoria</ion-label>
+                    </ion-item>
+                    <ion-item href="#">
+
+                        <ion-label>Nome da categoria</ion-label>
+                    </ion-item>
+
+
+                    <!-- <p>Nada a exibir</p> -->
+
+            </ion-list>
+
+            <!-- </ion-list> -->
+</ion-content>
+
+
+
+
             </ion-tab>
         <ion-tab tab="promocoes">
+        <img src="{{asset('vendor/argon/assets/img/brand/google.jpg')}}" style="width: 400px; height:200px; " class="top">
                 <ion-content>
-                <ion-list>
-                
-                    <ion-item>
-                        <ion-label></ion-label>
-                    </ion-item>
-                    <ion-item>
-                        <ion-label></ion-label>
-                    </ion-item>
-                    <ion-item>
-                        <ion-label>Pokémon Yellow</ion-label>
-                    </ion-item>
-                    <ion-item>
-                        <ion-label>Mega Man X</ion-label>
-                    </ion-item>
-                    <ion-item>
-                        <ion-label>The Legend of Zelda</ion-label>
-                    </ion-item>
-                    <ion-item>
-                        <ion-label>Pac-Man</ion-label>
-                    </ion-item>
-                   
+                <!-- <ion-list> -->
+
+
+                    <!-- <ion-item>
+                        <ion-label><img src="{{asset('vendor/argon/assets/img/brand/google.jpg')}}" style="width: 50px; height:50px;"> Nome da Promoção e R$</ion-label>
+                    </ion-item> -->
+                    <ion-content>
+
+                <ion-list style="margin-top: 5px">
+
+                        <ion-item href="#">
+                            <ion-thumbnail slot="start">
+                                <img src="{{asset('vendor/argon/assets/img/brand/google.jpg')}}" alt="">
+                            </ion-thumbnail>
+                            <ion-label>nome </br> R$ valor</ion-label>
+                        </ion-item>
+
+                        <ion-item href="#">
+                            <ion-thumbnail slot="start">
+                                <img src="{{asset('vendor/argon/assets/img/brand/google.jpg')}}" alt="">
+                            </ion-thumbnail>
+                            <ion-label>nome </br> R$ valor</ion-label>
+                        </ion-item>
+                        <ion-item href="#">
+                            <ion-thumbnail slot="start">
+                                <img src="{{asset('vendor/argon/assets/img/brand/google.jpg')}}" alt="">
+                            </ion-thumbnail>
+                            <ion-label>nome </br> R$ valor</ion-label>
+                        </ion-item>
+                        <ion-item href="#">
+                            <ion-thumbnail slot="start">
+                                <img src="{{asset('vendor/argon/assets/img/brand/google.jpg')}}" alt="">
+                            </ion-thumbnail>
+                            <ion-label>nome </br> R$ valor</ion-label>
+                        </ion-item>
+                        <ion-item href="#">
+                            <ion-thumbnail slot="start">
+                                <img src="{{asset('vendor/argon/assets/img/brand/google.jpg')}}" alt="">
+                            </ion-thumbnail>
+                            <ion-label>nome </br> R$ valor</ion-label>
+                        </ion-item>
+                        <ion-item href="#">
+                            <ion-thumbnail slot="start">
+                                <img src="{{asset('vendor/argon/assets/img/brand/google.jpg')}}" alt="">
+                            </ion-thumbnail>
+                            <ion-label>nome </br> R$ valor</ion-label>
+                        </ion-item>
+
+
+                        <!-- <p>Nada a exibir</p> -->
+
                 </ion-list>
+
+                <!-- </ion-list> -->
     </ion-content>
         </ion-tab>
 
         <ion-tab tab="parceiro">
-
-        <ion-item style="margin-top:50px">
-                <ion-label><h1>Propaganda</h1></ion-label>
-      
+            <img src="{{asset('vendor/argon/assets/img/brand/google.jpg')}}" style="width: 400px; height:200px;">
+            <ion-content>
                 
+                    <ion-content>
+        <!-- <ion-item style="margin-top:5px">
+                <ion-label> </ion-label>
+
+
             </ion-item>
             <ion-item style="margin-top:50px">
-                <ion-label><h1>{{ $menu->name }}</h1></ion-label>
-      
-                
+                <ion-label>
+
+                <h1>{{ $menu->name }}</h1></ion-label>
+
+
             </ion-item>
             <ion-item style="margin-top:50px">
                 <ion-label><h3>Feito por cardappion.me</h3></ion-label>
-                
-                
-            </ion-item>
+
+
+            </ion-item> -->
+
+            <ion-list style="margin-top: 5px">
+
+                       
+
+                        <ion-item href="#">
+                            <!-- <ion-thumbnail slot="start">
+                                <img src="{{asset('vendor/argon/assets/img/brand/google.jpg')}}" alt="">
+                            </ion-thumbnail> -->
+                            <ion-label>{{ $menu->name }}</ion-label>
+                        </ion-item>
+
+                        <ion-item href="#">
+                           
+                            <ion-label>Cidade - ESTADO
+                            </br>
+                            whatspp
+                            </br>
+                            instagram
+                            
+                            
+                            </ion-label>
+                            
+                        </ion-item>
+                        <ion-item style="margin-top:50px">
+                             <ion-label><h3>Feito por cardappion.me</h3></ion-label>
+
+
+                            </ion-item>
+                        
+
+
+                        <!-- <p>Nada a exibir</p> -->
+
+                </ion-list>
 
         </ion-tab>
 
@@ -105,7 +207,7 @@
                 <ion-icon name="gift"></ion-icon>
             </ion-tab-button>
 
-            
+
 
         </ion-tab-bar>
         </ion-tabs>
@@ -128,4 +230,3 @@
 </script>
 </body>
 </html>
-
