344af07dba170bf29386dd1f2a080d1efe085b14
pedro <pedro.nogueira@hotmail.com.br>
2019-11-19 17:55:00 -0300

    tests

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -8,6 +8,8 @@
   <!-- <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="Start your development with a Dashboard for Bootstrap 4.">
   <meta name="author" content="Creative Tim"> -->
+
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   
   <title>{{ config('app.name') }}</title>
 
