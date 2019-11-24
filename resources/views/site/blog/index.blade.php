c6aa1edd4c4723c870e19c18e0ec19b25280a859
Pedro Henrique Tolentino de Melo Nogueira <pedro.nogueira@hotmail.com.br>
2019-11-23 22:39:35 -0200

    testes

--- resources/views/site/blog/index.blade.php
+++ resources/views/site/blog/index.blade.php
@@ -4,7 +4,11 @@
 <html>
 
 <head>
-  <meta charset="utf-8">
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="Start your development with a Dashboard for Bootstrap 4.">
+  <meta name="author" content="Creative Tim">
+  <title>{{ config('app.name') }}</title>
   <title>{{ config('app.name') }}</title>
   <link rel="stylesheet" href="{{ asset('assets/css/stilo.css') }}" type="text/css">
   <link rel="icon" href="{{ asset('vendor/argon/assets/img/brand/logo3.png') }}" type="image/png">
--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -4,13 +4,10 @@
 <html>
 
 <head>
-  <meta charset="utf-8">
-  <!-- <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="Start your development with a Dashboard for Bootstrap 4.">
-  <meta name="author" content="Creative Tim"> -->
-
+<meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  
+  <meta name="description" content="Start your development with a Dashboard for Bootstrap 4.">
+  <meta name="author" content="Creative Tim">
   <title>{{ config('app.name') }}</title>
 
   <link rel="stylesheet" href="{{ asset('assets/css/stilo.css') }}" type="text/css">
